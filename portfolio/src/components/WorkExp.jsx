78e5924eb8131a16307a3504c41f7cec753b66ce
ShardulVanage <vsharadul@gmail.com>
2024-01-20 23:06:48 +0530

    about me completed

---
+++ portfolio/src/components/WorkExp.jsx
@@ -0,0 +1,49 @@
+
+export function WorkExp() {
+  return (
+    <section aria-labelledby=":r19:" class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+              <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+                <h2 id=":r19:" class="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
+                >Zep Analytics
+                </h2>
+                <div class="md:col-span-3"><ul role="list" class="space-y-8">
+                  <li class="group relative flex flex-col items-start">
+                    <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      Web Developer (Internship)
+                      </h3>
+                      <p class="relative z-10 mt-2 text-sm text-zinc-700 dark:text-zinc-300">
+                        Jul 2023 - Present · 3 mos</p>
+                        <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                          <ul class="w-full list-inside list-disc">
+                            <li>
+Developed and maintained the company's website for the
+course-selling platform. 
+                              </li>
+                               <li>
+                                 Created static pages to enhance the course-selling
+experience and improve user engagement.
+                                 </li>
+                                 <li>
+Collaborated with the development team to implement
+new features and functionalities. 
+                                 </li>
+                                 
+                                 </ul>
+                                 <div class="w-full flex flex-wrap mt-4 space-x-4">
+                                  <div class="my-2 rounded-full bg-white/90 px-3 text-sm font-medium text-teal-500 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-teal-400 dark:ring-white/10">
+                                    React</div>
+                                  
+                                    <div class="my-2 rounded-full bg-white/90 px-3 text-sm font-medium text-teal-500 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-teal-400 dark:ring-white/10">
+                                      Tailwind CSS
+                                      </div>
+                                
+                                      </div>
+                                      </p>
+                                      </li>
+                                      </ul>
+                                      </div>
+                                      </div>
+                                      </section>
+  )
+}
+
